8b1171ca3316a9546091968ec252f3a7fd83af43
claudim <claudimarco@hotmail.it>
2021-01-31 11:55:50 +0100

    Added input handling

--- UnivaqHexMeshMain.cpp
+++ UnivaqHexMeshMain.cpp
@@ -20,64 +20,81 @@ typedef CGAL::Mesh_polyhedron_3<K>::type Polyhedron;
 int main(int argc, char* argv[]) {
 
 //    const std::string data_path = "/Users/claudia/CLionProjects/3DMesher/DataInput";
-    const std::string data_path = "/home/claudia/CLionProjects/3DMesher/DataInput";
-    std::string name = "bolt";
-    //std::string name = "bunny";
-    //std::string name = "ellipse";
-    //std::string name = "Tronco_cono_3D";
-    //std::string name = "cube100x100axisAligned";
-    //std::string name = "cube100x100rotated";
-//    std::string name = "sfera";
-    //std::string name = "rotatedTrapezoid";
+////    const std::string data_path = "/home/claudia/CLionProjects/3DMesher/DataInput";
+//    std::string fileName_without_extension = "bolt";
+//    //std::string fileName_without_extension = "bunny";
+//    //std::string fileName_without_extension = "ellipse";
+//    //std::string fileName_without_extension = "Tronco_cono_3D";
+//    //std::string fileName_without_extension = "cube100x100axisAligned";
+//    //std::string fileName_without_extension = "cube100x100rotated";
+////    std::string name = "sfera";
+//    //std::string fileName_without_extension = "rotatedTrapezoid";
+//
+//    //std::string fileName = data_path + "/cubeTest.stl";
+//    //const char* fname = (argc>1)?argv[1]:"sfera.off";
+//    //std::string fileName = data_path + "/sphere.off";
+//    //std::string fileName_without_extension = "cube100x100rotated";
+//
+//   // std::string fileName_without_extension = "cube100x100axisAligned";
+//    std::string fileName = data_path + "/" + fileName_without_extension + ".stl";
 
-    //std::string fileName = data_path + "/cubeTest.stl";
-    //const char* fname = (argc>1)?argv[1]:"sfera.off";
-    //std::string fileName = data_path + "/sphere.off";
-    //std::string name = "cube100x100rotated";
+    if (argc == 3 || argc == 4) {
+        try {
 
-   // std::string name = "cube100x100axisAligned";
-    std::string fileName = data_path + "/" + name + ".stl";
+            //get the stl file fileName_without_extension
+            std::string inputPathFileName = argv[1]; // filename is path to filename with extension
 
-    try {
-        // Get starting timepoint
-      //  auto start_reading = std::chrono::high_resolution_clock::now();
+//            size_t startIndex  = inputPathFileName.find_last_of("/");
+//            std::string fileName_with_extension = inputPathFileName.substr(startIndex + 1,inputPathFileName.size());
+//            std::string fileName_without_extension = fileName_with_extension.substr(0,(fileName_with_extension.size()-4));
+//            std::string fileName_extension = fileName_with_extension.substr((fileName_with_extension.size()-3),(fileName_with_extension.size()));
 
-        // read input from file
-        //OFF_Reader reader;
-        STL_reader reader;
-        const Polyhedron polyhedron = reader.read(fileName);
+            size_t startIndex = inputPathFileName.find_last_of(".");
+            std::string fileName_extension = inputPathFileName.substr((startIndex + 1), (inputPathFileName.size()));
+            if (fileName_extension == "stl") {
 
-        // Get ending timepoint
+                // Get starting timepoint
+                //  auto start_reading = std::chrono::high_resolution_clock::now();
+
+                // read input from file
+                //OFF_Reader reader;
+                STL_reader reader;
+                const Polyhedron polyhedron = reader.read(inputPathFileName);
+
+                // Get ending timepoint
 //        auto stop_reading = std::chrono::high_resolution_clock::now();
 
-        // Get duration
+                // Get duration
 //        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_reading - start_reading);
 
 //        std::cout << "Time taken by read function is : "
 //             << duration.count() << " microseconds" << std::endl;
 
 
-        //CGAL::draw(polyhedron);
-        Grid_maker gridMaker = Grid_maker();
-        LCC_3 hex_mesh = gridMaker.make(polyhedron);
+                //CGAL::draw(polyhedron);
+                Grid_maker gridMaker = Grid_maker();
+                if (argc == 4) {
+                    gridMaker.set_resolution(std::stoi(argv[3]));
+                }
+                LCC_3 hex_mesh = gridMaker.make(polyhedron);
 //        std::cout<<"fatto"<<std::endl;
 
-        //remove external blocks
+                //remove external blocks
                 auto start_reading = std::chrono::high_resolution_clock::now();
 
 //        Initial_mesh_maker<External_block_remover> initialMeshMaker;
 //        initialMeshMaker.removeBlocks(hex_mesh, polyhedron);
-        External_block_remover externalBlockRemover = External_block_remover();
-        externalBlockRemover.removeBlocks(hex_mesh, polyhedron);
+                External_block_remover externalBlockRemover = External_block_remover();
+                externalBlockRemover.removeBlocks(hex_mesh, polyhedron);
                 auto stop_reading = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_reading - start_reading);
+                auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_reading - start_reading);
 
-        std::cout << "Time taken by remove block function is : "
-             << duration.count() << " microseconds" << std::endl;
-        std::cout<<"rimossi i blocchi esterni"<<std::endl;
+                std::cout << "Time taken by remove block function is : "
+                          << duration.count() << " microseconds" << std::endl;
+                std::cout << "rimossi i blocchi esterni" << std::endl;
 
-        hex_mesh.display_characteristics(std::cout);
-        std::cout<<""<<std::endl;
+                hex_mesh.display_characteristics(std::cout);
+                std::cout << "" << std::endl;
 
 //        //get the number of hex_mesh element
 //        int number_of_int_element = 0;
@@ -88,27 +105,28 @@ int main(int argc, char* argv[]) {
 //        }
 //        std::cout<<"Number of initial mesh element "<< number_of_int_element<<std::endl;
 
-        auto start_reading_connect = std::chrono::high_resolution_clock::now();
+                auto start_reading_connect = std::chrono::high_resolution_clock::now();
 
 
-        //fit on boundary blocks to polyhedron boundary
-        CGAL::Grid_boundary_connector gridBoundaryConnector;
-        gridBoundaryConnector.connect(hex_mesh, polyhedron);
+                //fit on boundary blocks to polyhedron boundary
+                CGAL::Grid_boundary_connector gridBoundaryConnector;
+                gridBoundaryConnector.connect(hex_mesh, polyhedron);
 
-        auto stop_reading_connect = std::chrono::high_resolution_clock::now();
-        auto duration_connect = std::chrono::duration_cast<std::chrono::microseconds>(stop_reading_connect - start_reading_connect);
+                auto stop_reading_connect = std::chrono::high_resolution_clock::now();
+                auto duration_connect = std::chrono::duration_cast<std::chrono::microseconds>(
+                        stop_reading_connect - start_reading_connect);
 
-        std::cout << "Time taken by connect block function is : "
-                  << duration_connect.count() << " microseconds" << std::endl;
+                std::cout << "Time taken by connect block function is : "
+                          << duration_connect.count() << " microseconds" << std::endl;
 
-        //delete element with Volume <= 0
-        Volume_Validator volumeValidator;
-        volumeValidator.delete_blocks_with_less_than_or_equal_to_zero(hex_mesh);
+                //delete element with Volume <= 0
+                Volume_Validator volumeValidator;
+                volumeValidator.delete_blocks_with_less_than_or_equal_to_zero(hex_mesh);
 
-        // Get ending timepoint
+                // Get ending timepoint
 //        auto stop = std::chrono::high_resolution_clock::now();
 
-        // Get duration
+                // Get duration
 //        auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start_reading);
 
 //        std::cout << "Time taken by mesh generation is : "
@@ -122,14 +140,16 @@ int main(int argc, char* argv[]) {
 //            number_of_element++;
 //        }
 //        std::cout<<"Number of element "<< number_of_element<<std::endl;
-        
-        Degenerate_element_finder degenerateElementFinder;
-        std::vector<Dart_handle> degenerateElements = degenerateElementFinder.find_degenerate_elements(hex_mesh);
-        std::cout<<"Degenerate elements are " << degenerateElements.size() <<std::endl;
-        std::cout<<"Pyramid elements are " << degenerateElementFinder.getPyramids().size() <<std::endl;
-        std::cout<<"Tet elements are " << degenerateElementFinder.getTetrahedra().size() <<std::endl;
-        std::cout<<"Wedge elements are " << degenerateElementFinder.getWedges().size() <<std::endl;
-        std::cout<<"Quadrilateral elements are " << degenerateElementFinder.getQuadrilaterals().size() <<std::endl;
+
+                Degenerate_element_finder degenerateElementFinder;
+                std::vector<Dart_handle> degenerateElements = degenerateElementFinder.find_degenerate_elements(
+                        hex_mesh);
+                std::cout << "Degenerate elements are " << degenerateElements.size() << std::endl;
+                std::cout << "Pyramid elements are " << degenerateElementFinder.getPyramids().size() << std::endl;
+                std::cout << "Tet elements are " << degenerateElementFinder.getTetrahedra().size() << std::endl;
+                std::cout << "Wedge elements are " << degenerateElementFinder.getWedges().size() << std::endl;
+                std::cout << "Quadrilateral elements are " << degenerateElementFinder.getQuadrilaterals().size()
+                          << std::endl;
 //       for(Dart_handle wedge : degenerateElementFinder.getWedges()) {
 //           std::cout << "Nuovo wedge " << std::endl;
 //           //Dart_handle wedge = degenerateElementFinder.getWedges().at(0);
@@ -153,28 +173,55 @@ int main(int argc, char* argv[]) {
 //        }
 
 
-        //output
-//        const std::string out_data_path = "/Users/claudia/CLionProjects/3DMesher/MeshOutput/UnivaqHexMeshOutput/";
-        const std::string out_data_path = "/home/claudia/CLionProjects/3DMesher/MeshOutput/UnivaqHexMeshOutput";
-        std::string outputFileName = out_data_path + "/" + name + "_8Res.mesh";
+                //output
+                if (argc == 3 || argc == 4) {
 
-        std::ofstream medit_file(outputFileName);
-        Writer writer;
-        writer.output_to_medit(medit_file,hex_mesh);
-        medit_file.close();
+                    std::string outputPathFileName = argv[2]; // filename is path to filename with extension of the output
+                    //size_t startIndex  = outputPathFileName.find_last_of("/");
+                    //std::string output_fileName_with_extension = outputPathFileName.substr(startIndex + 1,outputPathFileName.size());
+                    // std::string fileName_without_extension = output_fileName_with_extension.substr(0,(output_fileName_with_extension.size()-4));
+                    // std::string output_file_extension = output_fileName_with_extension.substr((output_fileName_with_extension.size()-3),(output_fileName_with_extension.size()));
+                    size_t startIndex = outputPathFileName.find_last_of(".");
+                    Writer writer;
+                    std::string output_file_extension = outputPathFileName.substr(startIndex + 1,
+                                                                                  outputPathFileName.size());
+                    if (output_file_extension == "vtk") {
+                        std::ofstream vtk_file(outputPathFileName);
+                        writer.output_to_legacy_vtk_ascii_unstructured(vtk_file, hex_mesh);
+                        vtk_file.close();
+                    }
+                    if (output_file_extension == "mesh") {
+                        std::ofstream medit_file(outputPathFileName);
+                        writer.output_to_medit(medit_file, hex_mesh);
+                        medit_file.close();
+                    }
 
-        outputFileName = out_data_path + "/" + name + "_8Res.vtk";
-        std::ofstream vtk_file(outputFileName);
-        writer.output_to_legacy_vtk_ascii_unstructured(vtk_file, hex_mesh);
 
-        //draw in cgal the final hex mesh
-       // CGAL::draw(hex_mesh);
+//                const std::string out_data_path = "/Users/claudia/CLionProjects/3DMesher/MeshOutput/UnivaqHexMeshOutput/";
+////        const std::string out_data_path = "/home/claudia/CLionProjects/3DMesher/MeshOutput/UnivaqHexMeshOutput";
+//                std::string outputFileName = out_data_path + "/" + fileName_without_extension + "_8Res.mesh";
+
+//                Writer writer;
+//                std::ofstream medit_file(outputFileName);
+//                writer.output_to_medit(medit_file, hex_mesh);
+//                medit_file.close();
+
+//                outputFileName = out_data_path + "/" + fileName_without_extension + "_8Res.vtk";
+//                std::ofstream vtk_file(outputFileName);
+//                writer.output_to_legacy_vtk_ascii_unstructured(vtk_file, hex_mesh);
+//                vtk_file.close();
+
+                }
+                //draw in cgal the final hex mesh
+                // CGAL::draw(hex_mesh);
 
 
-    }
-    catch (std::ios_base::failure e) {
-        std::cerr << "Exception opening/reading/closing file\n";
+            }
+        }
+        catch (std::ios_base::failure e) {
+            std::cerr << "Exception opening/reading/closing file\n";
+        }
     }
     return EXIT_SUCCESS;
-}
 
+}
